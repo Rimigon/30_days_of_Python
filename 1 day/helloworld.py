948596b75df7655f72285081214a8f55f1e31316
Rimigon <nikita.rad9@gmail.com>
2023-11-20 14:03:02 +0300

    1 day

---
+++ 1 day/helloworld.py
@@ -0,0 +1,29 @@
+# 1
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+# 2
+
+print("Nikita")
+print("Radonov")
+print("Russia")
+print("I am enjoying 30 days of python")
+
+# 3 Checking data types
+
+print(type(10))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Nikita"))
+print(type("Radonov"))
+print(type("Russia"))
+
+# 4
+
+print("Euclidian distance ", ((2 - 10) ** 2 + (3 - 8) ** 2) ** 0.5)
