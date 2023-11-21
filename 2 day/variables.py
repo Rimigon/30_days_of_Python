e660d5f3407859d111cd663b5af13aa267c44a20
Rimigon <nikita.rad9@gmail.com>
2023-11-21 14:52:27 +0300

    day 2

---
+++ 2 day/variables.py
@@ -0,0 +1,53 @@
+# Level 1
+
+# Day 2: 30 Days of python programming
+
+name = "Nikita"
+
+last_name = "Radonov"
+
+full_name = "Radonov Nikita"
+
+country = "Russia"
+
+city = "Voskresensk"
+
+age = 22
+
+year = 2023
+
+is_married, is_true, is_light_on = False, True, True
+
+#Level 2
+
+print(type(name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+
+print(len(name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+
+radius = input('Radius ')
+area_of_circle = 3.14 * radius ** 2
+
