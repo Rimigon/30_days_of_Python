6ce50648a2ca2b181751cb9f9d4211b9ad05a49d
Rimigon <nikita.rad9@gmail.com>
2023-11-22 17:34:22 +0300

    day 3

---
+++ 3 day/main.py
@@ -0,0 +1,23 @@
+# 💻 Exercises - Day 3
+
+age = 22
+
+height = 173.6
+
+complex_number = 3 + 5j
+
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+print("The area of the triangle is ", 0.5 * base * height)
+
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+print("The perimeter of the triangle is ", a + b + c)
+
+
+age = float(input("Enter number of years you have lived:"))
+print("You have lived for ", age * 365 * 24 * 60 * 60, " seconds.")
